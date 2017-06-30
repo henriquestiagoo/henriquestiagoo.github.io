ac6710717cc68caec9e230fad2e3abcd47e6fe45
henriquestiago <th.tk@hotmail.com>
2017-06-30 17:33:44 +0100

    up!!!

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$('a').click(function (e) {
+  e.stopPropagation();
+});
+
+$('.card').click(function () {
+  $('.card-front').toggleClass('card-flip');
+  $('.card-back').toggleClass('card-flip');
+});
